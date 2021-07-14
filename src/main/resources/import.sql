8d6c5b54fb7a5d93574f7c9b32c0bd79d39a6e43
JhonAp25 <apazalaricojhon25@gmail.com>
2021-07-14 17:23:35 -0500

    login con usuario

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,4 +7,5 @@ insert into grado(descripcion)  values ('Sexto');
 
 -- insert into alumno(nombre, apellido, telefono, dni, correo, estado, grado_id)  values ('Jhon Elvis', 'Apaza Larico', '999999999', '12345678', 'jhonAP@gmail.com', true, 1);
 insert into usuario values (null, 'admin001', 'Jhon Apaza' , 'admin01');
+insert into usuario values (null, 'admin002', 'Jhon Apaza2' , 'admin02');
 
